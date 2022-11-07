66ff4484a80a8324a682573a513113f2822610ad
Manuel López <manuel1n1@yahoo.com.mx>
2022-11-07 16:08:29 -0600

    (modify): enable user routes

--- src/main/kotlin/com/manuel1n1/plugins/Routing.kt
+++ src/main/kotlin/com/manuel1n1/plugins/Routing.kt
@@ -1,7 +1,7 @@
 package com.manuel1n1.plugins
 
 import com.manuel1n1.routes.loginRoutes
-//import com.manuel1n1.routes.userRoutes
+import com.manuel1n1.routes.userRoutes
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -11,7 +11,7 @@ fun Application.configureRouting() {
     routing {
         route("/api/v1") {
             loginRoutes()
-            //userRoutes()
+            userRoutes()
             get("/") {
                 call.respondText("Hello World!")
             }
