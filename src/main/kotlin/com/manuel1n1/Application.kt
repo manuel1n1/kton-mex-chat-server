320f63b38bbfde6427ef7a296dbbe6c23861bdd4
Manuel López <manuel1n1@yahoo.com.mx>
2022-11-07 16:54:04 -0600

    (fix): optimize imports

--- src/main/kotlin/com/manuel1n1/Application.kt
+++ src/main/kotlin/com/manuel1n1/Application.kt
@@ -1,8 +1,10 @@
 package com.manuel1n1
 
+import com.manuel1n1.plugins.configureRouting
+import com.manuel1n1.plugins.configureSecurity
+import com.manuel1n1.plugins.configureSerialization
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
-import com.manuel1n1.plugins.*
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
--- src/main/kotlin/com/manuel1n1/plugins/Security.kt
+++ src/main/kotlin/com/manuel1n1/plugins/Security.kt
@@ -3,12 +3,10 @@ package com.manuel1n1.plugins
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.http.*
-import io.ktor.server.auth.*
-import io.ktor.util.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
 
 fun Application.configureSecurity() {
     val secret = environment.config.property("jwt.secret").getString()
