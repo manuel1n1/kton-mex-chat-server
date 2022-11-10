1eed606b628a381a91901f865a1064e7dcb32c7f
Manuel López <manuel1n1@yahoo.com.mx>
2022-11-10 17:27:24 -0600

    (add): User dao and models

--- src/main/kotlin/com/manuel1n1/models/User.kt
+++ src/main/kotlin/com/manuel1n1/models/User.kt
@@ -1,25 +1,39 @@
 package com.manuel1n1.models
 
-import com.manuel1n1.utils.DateSerializer
+import com.manuel1n1.utils.TimestampSerializer
 import com.manuel1n1.utils.UUIDSerializer
 import kotlinx.serialization.Serializable
 import java.util.*
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.timestamp
+import java.sql.Timestamp
+import java.time.Instant
 
 @Serializable
 data class User(
     @Serializable(with = UUIDSerializer::class)
-    val id: UUID,
+    val id: UUID?,
     val email:String,
-    val password:String,
-    @Serializable(with = DateSerializer::class)
-    val createdAt: Date)
+    var password:String,
+    @Serializable(with = TimestampSerializer::class)
+    val createdAt: Timestamp?)
 
 @Serializable
 data class UserLogin(val userName: String, val password: String)
 
 @Serializable
-data class UserLoginResponse(val user: UserLogin, val token: String)
+data class UserData(
+    @Serializable(with = UUIDSerializer::class)
+    val id: UUID,
+    val email: String)
 
-val userDb = listOf(UserLogin(
-    "intelliJ", "123456"
-))
+@Serializable
+data class UserLoginResponse(val user: UserData, val token: String)
+
+object Users: Table() {
+    val id:Column<UUID> = uuid("id")
+    val email:Column<String> = varchar("email", 100)
+    val password:Column<String> = varchar("password", 100)
+    val createdAt:Column<Instant> = timestamp("created_at")
+}
